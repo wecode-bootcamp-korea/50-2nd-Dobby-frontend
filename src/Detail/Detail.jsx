98512ab3af3150b506bd2bbba4796f677e98debe
unknown <alswl8184@naver.com>
2023-10-24 21:32:01 +0900

    UI구현 완료

--- src/Detail/Detail.jsx
+++ src/Detail/Detail.jsx
@@ -1,8 +1,100 @@
 import React from 'react';
 import './Detail.scss';
-
+import deliveryFee from './배송정보.jpg';
 const Detail = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="Detail">
+        <div className="content-Wrapper">
+          <div className="container">
+            <div className="productBox">
+              <div>
+                <img
+                  className="proudctImage"
+                  src="https://d38cxpfv0ljg7q.cloudfront.net/admin_contents/detail/jirR-1677568807193-THUMB.jpg"
+                  alt="product"
+                ></img>
+              </div>
+              <div className="productName">
+                <h1>원피스 퍼즐 1000피스</h1>
+                <h2>너 내 동료가 되라!</h2>
+                <h2>평점 : 4.9점</h2>
+                <h3>50,000원</h3>
+                <div className="numberBox">
+                  <button className="quantity">+</button>
+                  <input
+                    className="quantity number"
+                    type="number"
+                    value="1"
+                  ></input>
+                  <button className="quantity">-</button>
+                </div>
+                <img className="deliveryFee" src={deliveryFee} alt="배송정보" />{' '}
+                <div>배송비 30,000원 이상 무료배송</div>
+                <br />
+                <input className="total" value="총 상품금액" />
+                <input className="total" value="50000" type="number" />
+                <button className="cart">장바구니</button>
+              </div>
+            </div>
+            <div className="detail-Box"></div>
+            <div className="division" />
+            <div className="review">
+              <input type="text" className="reviewPage" value="리뷰"></input>
+              <div className="review-Header">
+                <span>최민지</span>
+                <span>2023-10-24</span>
+              </div>
+              <div className="content">
+                루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤
+                diy키트가 올지 두근두근!루피가 너무 잘생겼고 쵸파가 너무
+                귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지 두근두근!루피가 너무
+                잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지
+                두근두근!루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔
+                어떤 diy키트가 올지 두근두근!
+              </div>
+              <div className="review-Header">
+                <span>최민지</span>
+                <span>2023-10-24</span>
+              </div>
+              <div className="content">
+                루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤
+                diy키트가 올지 두근두근!루피가 너무 잘생겼고 쵸파가 너무
+                귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지 두근두근!루피가 너무
+                잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지
+                두근두근!루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔
+                어떤 diy키트가 올지 두근두근!
+              </div>
+              <div className="review-Header">
+                <span>최민지</span>
+                <span>2023-10-24</span>
+              </div>
+              <div className="content">
+                루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤
+                diy키트가 올지 두근두근!루피가 너무 잘생겼고 쵸파가 너무
+                귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지 두근두근!루피가 너무
+                잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지
+                두근두근!루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔
+                어떤 diy키트가 올지 두근두근!
+              </div>
+              <div className="review-Header">
+                <span>최민지</span>
+                <span>2023-10-24</span>
+              </div>
+              <div className="content">
+                루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤
+                diy키트가 올지 두근두근!루피가 너무 잘생겼고 쵸파가 너무
+                귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지 두근두근!루피가 너무
+                잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔 어떤 diy키트가 올지
+                두근두근!루피가 너무 잘생겼고 쵸파가 너무 귀여워요 짱짱짱 다음엔
+                어떤 diy키트가 올지 두근두근!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-
+//리뷰: 닉네임,날짜,내용,평점
 export default Detail;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Detail from './Detail/Detail';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
