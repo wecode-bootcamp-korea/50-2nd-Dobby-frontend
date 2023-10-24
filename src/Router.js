975ead1a11ce2b44f8608aa7bee38714d2096e40
홍지영 <hjy961021@gmail.com>
2023-10-25 00:12:02 +0900

    배송지 목록 페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Delivery from './pages/Delivery/Delivery';
 import Login from './pages/Login/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/delivery" element={<Delivery />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
