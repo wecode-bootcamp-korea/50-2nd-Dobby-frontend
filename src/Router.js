5fcef94d12461778359719df98733245f8510f07
홍지영 <hjy961021@gmail.com>
2023-10-24 23:19:55 +0900

    1. 장바구니 페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
 import Login from './pages/Login/Login';
 
 const Router = () => {
@@ -7,6 +8,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
