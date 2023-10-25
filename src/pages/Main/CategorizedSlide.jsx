a4c133bbf7bfd627948767c0ac803a82b4375440
Haze10425 <jheee042500@gmail.com>
2023-10-25 21:11:04 +0900

    제품 리스트 슬라이드 브랜치 생성

---
+++ src/pages/Main/CategorizedSlide.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './categorizedSlide.scss';
+
+const CategorizedSlide = () => {
+  return <div className="categorizedSlide">제품 리스트 slide 레이아웃</div>;
+};
+
+export default CategorizedSlide;
