98528da13ab59c601ea697a5a0bc8bd8d5eeb7b3
김성호 <rlatjdgh9612@naver.com>
2023-10-26 12:45:26 +0900

    1. 이전 Signup-UI 브랜치 삭제후 main에서  새로운 브랜치 생성하기 - 완료 2. 이전 브랜치  작업 내용 이전하기 -  완료

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
