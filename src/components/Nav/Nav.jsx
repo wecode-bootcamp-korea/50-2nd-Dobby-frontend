5a96a4f7729294c42a2ac2428d4ff4f84ea28296
unknown <alswl8184@naver.com>
2023-10-27 13:47:46 +0900

    수정 완료

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CartLogo from './Cart-icon.png';
 import './Nav.scss';
-//로그인jsx에서 get.item 토큰을 받아와 nav에서
-//token과 일치한지 확인후 삼합연산자를 이용하여 onclick 이동하게하기
 
+//token과 일치한지 확인후 삼합연산자를 이용하여 onclick 이동하게하기
+//TOKEN STATE로 안해도 되고  localStorage에서만 가져와서 처리가능
+// 로그인시 localStorage.getItem('nickname')님 어서오세요 :) ,메시지 남기기
 const Nav = () => {
   const navigate = useNavigate();
+  const [login, setLogin] = useState(false);
+  const dobbyToken = localStorage.getItem('TOKEN');
+
+  useEffect(() => {
+    dobbyToken && setLogin(true);
+  }, [dobbyToken, login]);
+
+  const logout = () => {
+    localStorage.removeItem('TOKEN');
+    alert('로그아웃 되셨습니다!');
+    setLogin(false);
+  };
+
   return (
     <div className="navHeader">
       <div className="header">
-        <h1>DOBBY</h1>
+        <h1
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          DOBBY
+        </h1>
         <div
           className="navbarMenu"
           onClick={() => {
@@ -20,24 +40,29 @@ const Nav = () => {
           스토어
         </div>
       </div>
+
       <div className="login">
-        <label
-          onClick={() => {
-            navigate('/payment');
-          }}
-        >
-          로그인
-        </label>
-        |
-        <label
-          onClick={() => {
-            navigate('/cart');
-          }}
-        >
-          로그아웃
-        </label>
+        {login ? (
+          <label onClick={logout}> 로그아웃 </label>
+        ) : (
+          <label
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            로그인
+          </label>
+        )}
+
         <div>
-          <img className="cart" src={CartLogo} alt="장바구니" />
+          <img
+            className="cart"
+            src={CartLogo}
+            alt="장바구니"
+            onClick={() => {
+              navigate('/cart');
+            }}
+          />
         </div>
       </div>
     </div>
@@ -45,3 +70,20 @@ const Nav = () => {
 };
 
 export default Nav;
+
+/* <div className="login">
+<label
+  onClick={() => {
+    navigate('/payment');
+  }}
+>
+  로그인
+</label>
+|
+<label
+  onClick={() => {
+    navigate('/cart');
+  }}
+>
+  로그아웃
+</label> */
