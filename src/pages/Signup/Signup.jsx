46ff88e46e5c7972f5b0948d8f0b38bd5064cc21
김성호 <rlatjdgh9612@naver.com>
2023-10-28 16:26:15 +0900

    1. 회원가입 - 이메일, 비밀번호, 비밀번호 확인, 이름, 닉네임 유효성 검사 기능구현 - 완료 2. 회원가입 - 회원가입 입력창 width 값 350px로 수정 - 완료 3. 회원가입 - 서비스 이용약관 text, 체크박스 위치 왼쪽으로 수정- 완료

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,19 +1,103 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
+  // 이메일, 비밀번호, 비밀번호 확인, 이름, 닉네임 확인
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfir, setPasswordConfir] = useState('');
+  const [name, setName] = useState('');
+  const [nickname, setNickName] = useState('');
+  // (이메일, 비밀번호, 비밀번호 확인, 이름, 닉네임) 오류 메세지 상태 저장
+  const [emailMessage, setEmailMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirMessage, setPasswordConfirMessage] = useState('');
+  const [nameMessage, setNameMessage] = useState('');
+  const [nicknameMessage, setNickNameMessage] = useState('');
+  // (이메일, 비밀번호, 비밀번호 확인, 이름, 닉네임) 입력창 유효성 검사
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isPasswordConfir, setIsPasswordConfir] = useState(false);
+  const [isName, setIsName] = useState(false);
+  const [isNickName, setIsNickName] = useState(false);
   // 체크박스(state)
   const [allCheck, setAllcheck] = useState(false);
   const [useCheck, setUsecheck] = useState(false);
   const [infoCheck, setInfocheck] = useState(false);
-  // 회원가입 유효성 검사
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-    passwordConfir: '',
-    name: '',
-    nickname: '',
-  });
+
+  // 이메일
+  const onChangeEmail = useCallback(event => {
+    const emailRagex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = event.target.value;
+    setEmail(emailCurrent);
+    if (!emailRagex.test(emailCurrent)) {
+      setEmailMessage('올바른 이메일 형식이 아닙니다.');
+      setIsEmail(false);
+    } else {
+      setEmailMessage('올바른 이메일 형식입니다.');
+      setIsEmail(true);
+    }
+  }, []);
+  // 비밀번호
+  const onChangePassword = useCallback(event => {
+    const passwordRagex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-11]).{8,25}$/;
+    const passwordCurrent = event.target.value;
+    setPassword(passwordCurrent);
+    if (!passwordRagex.test(passwordCurrent)) {
+      setPasswordMessage(
+        '숫자+영문자+특수문자 조합으로 10자리 이상 입력해주세요.',
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage('안전한 비밀번호 입니다.');
+      setIsPassword(true);
+    }
+  }, []);
+  // 비밀번호 확인
+  const onChangePasswordConfir = useCallback(
+    event => {
+      const passwordConfirCurrent = event.target.value;
+      setPasswordConfir(passwordConfirCurrent);
+      if (password === passwordConfirCurrent) {
+        setPasswordConfirMessage('비밀번호가 일치합니다.');
+        setIsPasswordConfir(true);
+      } else {
+        setPasswordConfirMessage(
+          '비밀번호가 일치하지 않습니다. 다시한번 확인해주세요',
+        );
+        setIsPasswordConfir(false);
+      }
+    },
+    [password],
+  );
+  // 이름
+  const onChangeName = useCallback(event => {
+    const nameRagex = /^[ㄱ-ㅎ|가-힣]+$/;
+    const nameCurrent = event.target.value;
+    setName(nameCurrent);
+    if (!nameRagex.test(nameCurrent)) {
+      setNameMessage('올바른 이름 형식이 아닙니다.');
+      setIsName(false);
+    } else {
+      setNameMessage('올바른 이름 형식입니다.');
+      setIsName(true);
+    }
+  }, []);
+  // 닉네임
+  const onChangeNickName = useCallback(event => {
+    const nicknameRagex = /^[a-z|A-Z]+$/;
+    const nicknameCurrent = event.target.value;
+    setNickName(nicknameCurrent);
+    if (!nicknameRagex.test(nicknameCurrent)) {
+      setNickNameMessage('닉네임은 영어로 입력해주세요');
+      setIsNickName(false);
+    } else {
+      setNickNameMessage('올바른 닉네임 형식입니다.');
+      setIsNickName(true);
+    }
+  }, []);
   // 모두 동의 버튼
   const allBtnevent = () => {
     if (allCheck === false) {
@@ -58,43 +142,78 @@ const Signup = () => {
         <p className="subText">지금 회원가입하면 최대 4,000P를 드려요</p>
       </header>
       <form className="inputFrame">
-        <input
-          className="email"
-          type="text"
-          placeholder="이메일을 입력해주세요"
-          value={form.email}
-          onChange={event => setForm({ ...form, email: event.target.value })}
-        />
-        <input
-          className="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요"
-          value={form.password}
-          onChange={event => setForm({ ...form, password: event.target.value })}
-        />
-        <input
-          className="passwordConfir"
-          type="password"
-          placeholder="비밀번호를 다시한번 입력해주세요"
-          value={form.passwordConfir}
-          onChange={event =>
-            setForm({ ...form, passwordConfir: event.target.value })
-          }
-        />
-        <input
-          className="name"
-          type="text"
-          placeholder="이름을 입력해주세요"
-          value={form.name}
-          onChange={event => setForm({ ...form, name: event.target.value })}
-        />
-        <input
-          className="nickname"
-          type="text"
-          placeholder="닉네임을 입력해주세요"
-          value={form.nickname}
-          onChange={event => setForm({ ...form, nickname: event.target.value })}
-        />
+        <div className="formbox">
+          <input
+            className="email"
+            type="text"
+            typeName="email"
+            onChange={onChangeEmail}
+            placeholder="이메일을 입력해주세요"
+          />
+          {email.length > 0 && (
+            <span className={`message ${isEmail ? 'success' : 'error'}`}>
+              {emailMessage}
+            </span>
+          )}
+        </div>
+        <div className="formbox">
+          <input
+            className="password"
+            type="password"
+            onChange={onChangePassword}
+            placeholder="비밀번호를 입력해주세요"
+            maxLength={10}
+          />
+          {password.length > 0 && (
+            <span className={`message ${isPassword ? 'success' : 'error'}`}>
+              {passwordMessage}
+            </span>
+          )}
+        </div>
+        <div className="formbox">
+          <input
+            className="passwordConfir"
+            type="password"
+            onChange={onChangePasswordConfir}
+            placeholder="비밀번호를 다시한번 입력해주세요"
+            maxLength={10}
+          />
+          {passwordConfir.length > 0 && (
+            <span
+              className={`message ${isPasswordConfir ? 'success' : 'error'}`}
+            >
+              {passwordConfirMessage}
+            </span>
+          )}
+        </div>
+        <div className="formbox">
+          <input
+            className="name"
+            type="text"
+            typeName="name"
+            onChange={onChangeName}
+            placeholder="이름을 입력해주세요"
+          />
+          {name.length > 0 && (
+            <span className={`message ${isName ? 'success' : 'error'}`}>
+              {nameMessage}
+            </span>
+          )}
+        </div>
+        <div className="formbox">
+          <input
+            className="nickname"
+            type="text"
+            typeName="nickname"
+            onChange={onChangeNickName}
+            placeholder="닉네임을 입력해주세요"
+          />
+          {nickname.length > 0 && (
+            <span className={`message ${isNickName ? 'success' : 'error'}`}>
+              {nicknameMessage}
+            </span>
+          )}
+        </div>
       </form>
       <p className="coditionsText">Dobby 서비스 이용약관에 동의해주세요.</p>
       <span className="checkboxFrame01">
