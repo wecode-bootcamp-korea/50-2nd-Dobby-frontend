acb762411e937bbc820ed9aab48bdfb7e997ab48
김성호 <rlatjdgh9612@naver.com>
2023-10-31 22:52:07 +0900

    1. input self closing 수정 완료 2. form 태그 div 적용 완료 3. SCSS 여백 수정 완료

--- src/pages/FindID/FindID.jsx
+++ src/pages/FindID/FindID.jsx
@@ -6,16 +6,20 @@ const FindID = () => {
     <div className="idFindFrame">
       <h1 className="idText">아이디 찾기</h1>
       <form className="findInputFrame">
-        <input
-          className="nameInput"
-          type="text"
-          placeholder="이름을 입력해주세요"
-        ></input>
-        <input
-          className="phoneInput"
-          type="text"
-          placeholder="휴대폰 번호를 입력해주세요 (- 제외)"
-        ></input>
+        <div>
+          <input
+            className="nameInput"
+            type="text"
+            placeholder="이름을 입력해주세요"
+          />
+        </div>
+        <div>
+          <input
+            className="phoneInput"
+            type="text"
+            placeholder="휴대폰 번호를 입력해주세요 (- 제외)"
+          />
+        </div>
       </form>
       <div className="IDButtonFrame">
         <button className="findIDButton">아이디 찾기</button>
