2cd04e48904abfec0aeed6632c70833c059319cb
홍지영 <hjy961021@gmail.com>
2023-11-01 19:50:10 +0900

    구매하기 버튼 서버 확인

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const BASE_URL = 'http://10.58.52.105:8000';
-const BASE_URL2 = 'http://10.58.52.239:8000';
+const BASE_URL2 = 'http://10.58.52.244:8000';
+const BASE_URL3 = 'http://10.58.52.67:8000';
 const PRODUCT_URL = 'http://10.58.52.239:8000';
 
 export const GET_SUBSCRIPTION_API = `${BASE_URL}/subscription`;
@@ -8,5 +9,6 @@ export const GET_PRODUCTLIST_API = `${BASE_URL}/productlist`;
 
 export const GET_PRODUCT_API = `${PRODUCT_URL}`;
 
+export const GET_MOCK_API = `/data/data.json`;
 export const GET_CART_API = `${BASE_URL2}/cart`;
-export const GET_CART_PAYMENT_API = `${BASE_URL2}/cart/payment`;
+export const GET_CART_PAYMENT_API = `${BASE_URL3}/cart`;
