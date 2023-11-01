b431a19d6338c2a8c2b22d5256526d4806041702
김성호 <rlatjdgh9612@naver.com>
2023-11-01 17:27:09 +0900

    1. 로그인 UI 수정 2. 로그인 페이지에 아이디 찾기 버튼 클릭시 FindID 페이지로 이동

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  // ID(이메일)
+  const [id, setID] = useState('');
+  const saveUserID = event => {
+    setID(event.target.value);
+  };
+  // PW(비밀번호)
+  const [pw, setPW] = useState('');
+  const saveUserPW = event => {
+    setPW(event.target.value);
+  };
+  // 로그인 버튼
+  const isInvalid = id.includes('@', '.') && pw.length >= 10;
+  // 회원가입 버튼(회원가입 페이지로 이동)
+  const navigate = useNavigate();
+  const goToSignup = () => {
+    navigate('/signup');
+  };
+  const goToFindID = () => {
+    navigate('/findid');
+  };
+
+  const goToMain = () => {
+    fetch('http://10.58.52.105:8000/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: id,
+        password: pw,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message === 'LOG_IN_SUCCESS') {
+          alert('로그인 되었습니다.');
+          localStorage.setItem('token', data.token);
+        } else if (data.message === 'INVALID EMAIL OR PASSWORD') {
+          alert('가입되지 않은 정보입니다.');
+        }
+        console.log(data);
+      });
+  };
+
   return (
     <div className="mainLoginBody">
       <h1 className="helloText">안녕하세요!😊</h1>
@@ -10,17 +55,36 @@ const Login = () => {
         <h2 className="loginText">LOGIN</h2>
       </div>
       <div className="inputFrame">
-        <input className="userInput" type="text" placeholder="이메일" />
-        <input className="userInput" type="text" placeholder="비밀번호" />
-        <button className="loginButton">로그인</button>
-        <hr className="underline" />
+        <input
+          className="idInput"
+          type="text"
+          onChange={saveUserID}
+          placeholder="이메일"
+        />
+        <input
+          className="pwInput"
+          type="password"
+          onChange={saveUserPW}
+          placeholder="비밀번호"
+        />
+        <button
+          className={isInvalid ? 'loginButton' : 'disabledButton'}
+          disabled={isInvalid ? false : true}
+          onClick={goToMain}
+        >
+          로그인
+        </button>
       </div>
       <div className="idpwButtonFrame">
-        <button className="idButton">아이디 찾기</button>
+        <button className="idButton" onClick={goToFindID}>
+          아이디 찾기
+        </button>
         <button className="pwButton">비밀번호 찾기</button>
       </div>
       <div className="signupButtonFrame">
-        <button className="signupButton">회원가입</button>
+        <button className="signupButton" onClick={goToSignup}>
+          회원가입
+        </button>
       </div>
     </div>
   );
